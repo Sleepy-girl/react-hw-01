54ea1f0cc76f3b2432365533f47809484ed0818b
nyuta <pillowforneedles@gmail.com>
2020-08-13 13:05:48 +0300

    test

---
+++ build/precache-manifest.e9f3108888f22c419b4c6fb7f009a705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea88ded7a97e12f8da979e233cfd1ca",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "f5f2b70af200446fe5ae",
+    "url": "/goit-react/static/css/main.9f40043b.chunk.css"
+  },
+  {
+    "revision": "115220339b3af529a639",
+    "url": "/goit-react/static/js/2.c8845a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react/static/js/2.c8845a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f2b70af200446fe5ae",
+    "url": "/goit-react/static/js/main.eb316ff3.chunk.js"
+  },
+  {
+    "revision": "bc74bc948b887ae60e34",
+    "url": "/goit-react/static/js/runtime-main.ba0ba1f7.js"
+  }
+]);
